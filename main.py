9727f19232d9af10ea00174447ce5cc0a85bd56a
Ali Nouri <dynamic11@gmail.com>
2019-12-19 14:24:54 -0500

    generatied MLP model to predict success with plots

--- main.py
+++ main.py
@@ -1,30 +1,114 @@
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
+import seaborn as sns
 
-df = pd.read_csv('data/bank-additional-full.csv',sep=';')
-df.head()
+DEBUG = True
 
+# read data form file CSV into pandas data structure
+data = pd.read_csv('data/bank-additional-full.csv', sep=';')
+
+# This attribute highly affects the output target (e.g., if duration=0 then y=no). Yet, the duration is not known
+# before a call is performed. Also, after the end of the call, y is obviously known. Thus, this input should only be
+# included for benchmark purposes and should be discarded if the intention is to have a realistic predictive model.
+
+# drop pday becasue data seems to be inconsistent (ex. pdays says person has not been contacted but previous contacts
+# count is greater than 1)
 
 LE = LabelEncoder()
 #Work on the Categorical Values
-df['job_code'] = LE.fit_transform(df['job'])
-df['marital_code'] = LE.fit_transform(df['marital'])
-df['education_code'] = LE.fit_transform(df['education'])
-df['housing_code'] = LE.fit_transform(df['housing'])
-df['loan_code'] = LE.fit_transform(df['loan'])
-df['contact_code'] = LE.fit_transform(df['contact'])
-df['poutcome_code'] = LE.fit_transform(df['poutcome'])
-df['day_of_week_code'] = LE.fit_transform(df['day_of_week']) #maybe
-df['month_code'] = LE.fit_transform(df['month']) #maybe
-df['subscribed'] = LE.fit_transform(df['y'])
+data['job_code'] = LE.fit_transform(data['job'])
+data['marital_code'] = LE.fit_transform(data['marital'])
+data['education_code'] = LE.fit_transform(data['education'])
+data['housing_code'] = LE.fit_transform(data['housing'])
+data['loan_code'] = LE.fit_transform(data['loan'])
+data['contact_code'] = LE.fit_transform(data['contact'])
+data['poutcome_code'] = LE.fit_transform(data['poutcome'])
+data['day_of_week_code'] = LE.fit_transform(data['day_of_week']) #maybe
+data['month_code'] = LE.fit_transform(data['month']) #maybe
+data['subscribed'] = LE.fit_transform(data['y'])
 # Drop columns
-df=df.drop(['job','marital','education','housing','loan','contact','poutcome','day_of_week','month','y'] ,axis=1)
 
-y = df['subscribed']
-X = df.drop('subscribed',axis=1)
+data=data.drop(['job', 'marital', 'education', 'housing', 'loan', 'contact', 'poutcome', \
+                'day_of_week', 'month', 'default', 'y'], axis=1)
 
-# Set seed for reproducibility
-np.random.seed(42)
+if DEBUG:
+    export_csv = data.to_csv (r'cleaned_input_data.csv', index = None, header=True)
+
+
+#break up intput and output data
+y = data['subscribed']
+X = data.drop('subscribed', axis=1)
+
+selector = [
+    "age",
+    "pdays",
+    "campaign",
+    "previous",
+    "emp.var.rate",
+    "cons.price.idx",
+    "cons.conf.idx",
+    "euribor3m",
+    "nr.employed",
+    "job_code",
+    "marital_code",
+    "education_code",
+    "housing_code",
+    "loan_code",
+    "contact_code",
+    "poutcome_code",
+    "day_of_week_code",
+    "month_code",
+]
+
+
 # divide out the given training data into training (80%) and validation (20%)
-X_train, X_validate, y_train, y_validate = train_test_split(train[selector], train['Difference'], test_size=0.2, random_state=42)
+X_train, X_validate, y_train, y_validate = train_test_split(data[selector], data['subscribed'], test_size=0.3, random_state=42)
+
+if DEBUG:
+    X_train.to_csv(r'X_train.csv', index=None, header=True)
+
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+# Fit the Training Data
+scaler.fit(X_train)
+X_train = scaler.transform(X_train)
+X_validate = scaler.transform(X_validate)
+
+if DEBUG:
+    data.to_csv(r'X_train_scaled.csv', index=None, header=True)
+
+
+from sklearn.neural_network import MLPClassifier
+mlp = MLPClassifier(hidden_layer_sizes=(17,10,1),max_iter=500, activation = 'relu',solver='adam',  verbose=DEBUG, random_state=1)
+mlp.fit(X_train, y_train)
+
+
+import matplotlib.pyplot as plt
+plt.plot(mlp.loss_curve_,  label='Model Error vs Epoch')
+plt.title('Learning Loss Function')
+plt.xlabel('Loss')
+plt.ylabel('Epoch')
+plt.savefig("output/accuracy_vs_epoch.png", bbox_inches='tight', dpi=200, pad_inches=0.5)
+plt.close()
+
+
+predictions =mlp.predict(X_validate)
+from sklearn.metrics import classification_report,confusion_matrix
+cnf_matrix = confusion_matrix(y_validate,predictions)
+
+print(cnf_matrix);
+
+fig, ax = plt.subplots(1)
+ax = sns.heatmap(cnf_matrix, ax=ax, cmap=plt.cm.Greens, annot=True)
+#ax.set_xticklabels(abbreviation)
+#ax.set_yticklabels(abbreviation)
+plt.title('Confusion matrix of random forest predictions')
+plt.ylabel('True category')
+plt.xlabel('Predicted category')
+plt.savefig("output/Confusion_Matrix.png", bbox_inches='tight', dpi=200, pad_inches=0.5)
+plt.close()
+
+from sklearn.metrics import accuracy_score
+score = accuracy_score(y_validate,predictions)
+print(score)
